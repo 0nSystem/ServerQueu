504e3817740f71b0cf4cceebf37b6189fef2cbd0
onsystem <onsystem@gmail.com>
2022-06-01 08:43:59 +0200

    pruebas server queu

---
+++ TresEnRayaApp/HandlerSessionListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TresEnRayaApp
+{
+    public class HandlerSessionListener
+    {
+        public List<Session> Sessions = new List<Session>();
+
+        public HandlerSessionListener()
+        {
+
+        }
+        public void AddClient(TcpClient tcpClient)
+        {
+            if (Sessions.Count==0|Sessions[Sessions.Count].CompleteClients())
+            {
+                Session session = new Session();
+                session.AddClient(tcpClient);
+                Sessions.Add(session);
+            }
+            else
+            {
+                Sessions[Sessions.Count].AddClient(tcpClient);
+            }
+
+        }
+
+        
+    }
+}
---
+++ TresEnRayaApp/ServerQueu/Session.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace TresEnRayaApp
+{
+    public class Session
+    {
+        public TcpClient? Client1 { get; set; }
+        public TcpClient? Client2 { get; set; }
+
+        public Session()
+        {
+
+        }
+
+        public void AddClient(TcpClient tcpClient)
+        {
+            if (Client1==null)
+            {
+                Client1 = tcpClient;
+                return;
+            }
+            if (Client2==null)
+            {
+                Client2 = tcpClient;
+                return;
+            }
+        }
+        public bool CompleteClients()
+        {
+            if (Client1!=null&&Client2!=null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
