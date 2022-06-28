6f522da77901a9af060cd5a6384e1cc09188a4b0
onsystem <onsystem@gmail.com>
2022-06-28 18:03:36 +0200

    cambio organizacion carpeta Tasks

---
+++ ServerQueueApp/ServerQueu/Tasks/ITaskServerQueu.cs
@@ -0,0 +1,16 @@
+﻿using ServerQueu.Sessions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TresEnRayaApp;
+
+namespace ServerQueu.Services
+{
+    public interface ITaskServerQueu<T> where T : SessionInfo
+    {
+        bool RunTask();
+        bool AuthenticationBeforeTask();
+    }
+}
